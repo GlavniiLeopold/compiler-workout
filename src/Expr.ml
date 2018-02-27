018b806d8a056e9d218fea97d8e77e21e1e801ea
GlavniiLeopold <glavnii_leopold@mail.ru>
2018-02-27 21:09:43 +0300

    Update Expr.ml

--- src/Expr.ml
+++ src/Expr.ml
@@ -36,13 +36,17 @@ let update x v s = fun y -> if x = y then v else s y
 let s = update "x" 1 @@ update "y" 2 @@ update "z" 3 @@ update "t" 4 empty
 
 (* Some testing; comment this definition out when submitting the solution. *)
+
+(*
+
+
 let _ =
   List.iter
     (fun x ->
        try  Printf.printf "%s=%d\n" x @@ s x
        with Failure s -> Printf.printf "%s\n" s
     ) ["x"; "a"; "y"; "z"; "t"; "b"]
-
+*)
 (* Expression evaluator
 
      val eval : state -> expr -> int
@@ -50,5 +54,37 @@ let _ =
    Takes a state and an expression, and returns the value of the expression in 
    the given state.
 *)
+(*
 let eval = failwith "Not implemented yet"
+*)
+
+let rec eval_ex op x y = 
+    let toInt x = 
+		if x = 0 then false else true
+    and toInt op_bool x y =
+		if op_bool x y then 1 else 0
+    in
+    match op with
+    | "+" -> x + y
+    | "-" -> x - y
+    | "*" -> x * y
+    | "/" -> x / y
+    | "%" -> x mod y
+    | "<" -> toInt (<) x y
+    | "<=" -> toInt (<=) x y
+    | ">" -> toInt (>) x y
+    | ">=" -> toInt (>=) x y
+    | "==" -> toInt (==) x y
+    | "!=" -> toInt (<>) x y
+    | "&&" -> toInt (&&) (toBool x) (toBool y)
+    | "!!" -> toInt (||) (toBool x) (toBool y)
+
+let rec eval st ex = 
+    match ex with
+    | Const (value) -> value
+    | Var (value) -> st value
+    | Binop (oper, ex1, ex2) ->
+    let val1 = eval st ex1 
+    and val2 = eval st ex2 
+    in eval_ex op val1 val2
                     
