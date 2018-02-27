9d5a9418445504889863cc9e709b1c9976294e3f
GlavniiLeopold <glavnii_leopold@mail.ru>
2018-02-27 23:03:32 +0300

    Update Syntax.ml

--- src/Syntax.ml
+++ src/Syntax.ml
@@ -41,7 +41,35 @@ module Expr =
        Takes a state and an expression, and returns the value of the expression in 
        the given state.
     *)
-    let eval _ = failwith "Not implemented yet"
+    let rec eval_ex op x y = 
+    let toBool x = 
+		if x = 0 then false else true
+    and toInt op_bool x y =
+		if op_bool x y then 1 else 0
+    in
+    match op with
+    | "+" -> x + y
+    | "-" -> x - y
+    | "*" -> x * y
+    | "/" -> x / y
+    | "%" -> x mod y
+    | "<" -> toInt (<) x y
+    | "<=" -> toInt (<=) x y
+    | ">" -> toInt (>) x y
+    | ">=" -> toInt (>=) x y
+    | "==" -> toInt (==) x y
+    | "!=" -> toInt (<>) x y
+    | "&&" -> toInt (&&) (toBool x) (toBool y)
+    | "!!" -> toInt (||) (toBool x) (toBool y)
+
+let rec eval st ex = 
+    match ex with
+    | Const (value) -> value
+    | Var (value) -> st value
+    | Binop (oper, ex1, ex2) ->
+    let val1 = eval st ex1 
+    and val2 = eval st ex2 
+in eval_ex op val1 val2
 
   end
                     
@@ -65,7 +93,17 @@ module Stmt =
 
        Takes a configuration and a statement, and returns another configuration
     *)
-    let eval _ = failwith "Not implemented yet"
+let rec eval ((s, i, o) : config) stmt =
+        match stmt with
+        Read (x) -> 
+        (match i with 
+        [] -> failwith "Empty stream"
+        | num :: tail -> ((Expr.update x num s), tail, o))
+        | Write (expr) -> (s, i, o @ [(Expr.eval s expr)])
+        | Assign (x, expr) -> ((Expr.update x (Expr.eval s expr) s), i, o)
+        | Seq (stmt_fst, stmt_snd) -> 
+        let value_fst = eval (s, i, o) stmt_fst in
+eval value_fst stmt_snd;;
                                                          
   end
 
